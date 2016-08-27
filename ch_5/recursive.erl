a68f470bd6a0d7a7bbcd03ace3d6edabd0cf2856
Logan Hasson <logan.hasson@gmail.com>
2016-08-27 18:30:03 -0400

    Add duplicate/2

--- ch_5/recursive.erl
+++ ch_5/recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, tail_fac/1, len/1, tail_len/1]).
+-export([fac/1, tail_fac/1, len/1, tail_len/1, duplicate/2, tail_duplicate/2]).
 
 fac(1) -> 1;
 fac(N) -> N*fac(N-1).
@@ -12,5 +12,16 @@ len([]) -> 0;
 len([_|T]) -> 1 + len(T).
 
 tail_len(L) -> tail_len(L, 0).
+
 tail_len([], Len) -> Len;
 tail_len([_|T], Len) -> tail_len(T, Len + 1).
+
+duplicate(0, _) -> [];
+duplicate(N, Term) when N > 0 ->
+  [Term|duplicate(N-1, Term)].
+
+tail_duplicate(N, Term) -> tail_duplicate(N, Term, []).
+
+tail_duplicate(0, _, Acc) -> Acc;
+tail_duplicate(N, Term, Acc) when N > 0 ->
+  tail_duplicate(N-1, Term, [Term|Acc]).
