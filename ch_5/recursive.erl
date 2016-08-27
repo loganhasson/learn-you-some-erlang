ce9ed1571a2a55a80255e74e176d341612a7f491
Logan Hasson <logan.hasson@gmail.com>
2016-08-27 18:47:29 -0400

    Add comment

--- ch_5/recursive.erl
+++ ch_5/recursive.erl
@@ -40,7 +40,7 @@ sublist(_, 0) -> [];
 sublist([], _) -> [];
 sublist([H|T], N) -> [H|sublist(T,N-1)].
 
-tail_sublist(L, N) -> tail_reverse(tail_sublist(L, N, [])).
+tail_sublist(L, N) -> tail_reverse(tail_sublist(L, N, [])). % Could use lists:reverse/1 here.
 
 tail_sublist(_, 0, Acc) -> Acc;
 tail_sublist([], _, Acc) -> Acc;
